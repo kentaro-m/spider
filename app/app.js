800947b4a6dec3857ab6d62f5ea2345d9daddac0
Kentaro Matsushita <kentaro.m811@gmail.com>
2019-04-20 14:04:29 +0900

    Delete unused files

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 const createError = require('http-errors')
 const express = require('express')
-const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
@@ -8,15 +7,10 @@ const indexRouter = require('./routes/index')
 
 const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 
@@ -33,7 +27,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500)
-  res.render('error')
+  res.send(err.message)
 })
 
 module.exports = app
