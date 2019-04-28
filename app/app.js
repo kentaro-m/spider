6cecb6e8dc6aa66c32e8d25f035f3de441002f7a
Kentaro Matsushita <kentaro.m811@gmail.com>
2019-04-28 23:53:31 +0900

    Revert "Add a cron job to fetch and add articles"

--- app/app.js
+++ app/app.js
@@ -1,18 +1,33 @@
+const createError = require('http-errors')
 const express = require('express')
-const CronJob = require('cron').CronJob;
-const ArticlesModel = require('./models/articles_model')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+
+const indexRouter = require('./routes/index')
 
 const app = express()
 
-new CronJob('0 * * * *', async () => {
-  try {
-    const articlesModel = new ArticlesModel()
-    const results = await articlesModel.add()
-    console.log('success to add new articles')
-    console.log(results)
-  } catch (error) {
-    console.log(error.message)
-  }
-}, null, true, 'Asia/Tokyo');
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/', indexRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.send(err.message)
+})
 
 module.exports = app
