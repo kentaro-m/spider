9ffc61edb7516b11b21f703b47e3bc7a13861ca1
Kentaro Matsushita <kentaro.m811@gmail.com>
2019-04-28 22:32:55 +0900

    Add configuration to connect CloudSQL

--- api/driver/driver.go
+++ api/driver/driver.go
@@ -2,21 +2,9 @@ package driver
 
 import (
 	"database/sql"
-	"fmt"
-
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func ConnectDB(host, port, userName, password, dbName, dbCharset string) (*sql.DB, error) {
-	dbSource := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
-		userName,
-		password,
-		host,
-		port,
-		dbName,
-		dbCharset,
-	)
-
-	return sql.Open("mysql", dbSource)
+func ConnectDB(dsn string) (*sql.DB, error) {
+	return sql.Open("mysql", dsn)
 }
